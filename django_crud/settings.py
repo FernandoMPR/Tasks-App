ce77675eab2d268730ad310e062632a245b3116e
Reds <119710150+FernandoMPR@users.noreply.github.com>
2023-06-27 18:32:57 -0600

    Add files via upload
    New Database, postgresql

--- django_crud/settings.py
+++ django_crud/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'django_crud.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Name Data base",
+        "USER": "postgres",
+        "PASSWORD": "You password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
